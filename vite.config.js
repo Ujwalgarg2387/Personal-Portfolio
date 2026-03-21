b9900d97518043ec5cca6f1128fc2ec2c8f72e6d
UjwalGarg2387 <ujwalgarg2387@gmail.com>
2026-03-21 12:16:41 +0530

    sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import sitemap from 'vite-plugin-sitemap';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemap({ hostname: 'https://ujwal-garg-portfolio.netlify.app' })
+  ],
 });
